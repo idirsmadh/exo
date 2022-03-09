475463f0fb2008e71692f0116b8489add4619fa2
idirsmadh <101275968+idirsmadh@users.noreply.github.com>
2022-03-09 17:11:24 +0100

    mise a jour du code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function longueur(){
 
-    var str = document.getElementById('str').value;
-    console.log("Vous avez saisi ce mot " + ` ${str} ` + "il a " + `${str.length}` +" caracteres");
+    var ooo = document.getElementById('ooo').value;
+    console.log("Vous avez saisi ce mot " + ` ${ooo} ` + "il a " + `${ooo.length}` +" caracteres");
 
 }
